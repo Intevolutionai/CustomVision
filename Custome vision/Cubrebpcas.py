4c6d56badf0ac855098c302437b87986334e73af
Intevolutionai <145523615+Intevolutionai@users.noreply.github.com>
2023-09-19 22:01:41 -0600

    Custom Vision
    Aquí encuentras las imágenes y el código

---
+++ Custome vision/Cubrebpcas.py
@@ -0,0 +1,18 @@
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from msrest.authentication import ApiKeyCredentials
+import os, time, uuid
+
+prediction_key = "llenar con tus datos"
+ENDPOINT = "llenar con tus datos"
+project_id = "llenar con tus datos"
+publish_iteration_name = "llenar con tus datos"
+
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
+
+with open("C:/Users/Kevin/Desktop/Custome vision/Ima cubrebocas/descarga.jpg", mode="rb") as test_data:
+    results = predictor.detect_image(project_id, publish_iteration_name, test_data)
+
+for prediction in results.predictions:
+    print("\t" + prediction.tag_name + ": {0:.2f}% bbox.left = {1:.2f}, bbox.top = {2:.2f}, bbox.width = {3:.2f}, bbox.height = {4:.2f}".format(prediction.probability * 100, prediction.bounding_box.left, prediction.bounding_box.top, prediction.bounding_box.width, prediction.bounding_box.height))
+
